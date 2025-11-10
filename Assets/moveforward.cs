e3b590e35abf6566e138b8cebd0b80cd4662802a
thavaneshb <thavaneshb2006@gmail.com>
2025-11-10 09:21:07 +0530

    Add files via upload

---
+++ Assets/moveforward.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class moveforward : MonoBehaviour
+{
+    public float speed = 10.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+    }
+}
---
+++ Assets/playermoment.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class playermoment : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 5.0f;
+    public GameObject projectilePrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position =new Vector3(-xRange,transform.position.y,transform.position.z);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab,transform.position,projectilePrefab.transform.rotation);
+        }
+    }
+}
